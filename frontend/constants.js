46c6ec362ca934fc55633c3303bc8d4c46262008
Sergio <sergio2098crypto@gmail.com>
2022-09-06 11:11:05 +0300

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/sergio2098/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xfd8DfbDa3c8b4DB2b19C91D9D3254971da772Ba4";
